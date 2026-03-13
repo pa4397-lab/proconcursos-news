4324b757c2a5b92b454a67821873dc1572d9022a
Pianta PA <pa4397@gmail.com>
2026-03-13 17:55:22 -0400

    Update Home.jsx

--- apps/web/src/pages/Home.jsx
+++ apps/web/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 const SUPABASE_URL = "https://svfrmghbnyzkaorpnlqq.supabase.co"
-const SUPABASE_KEY = "COLE_SUA_ANON_KEY"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2ZnJtZ2hibnl6a2FvcnBubHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxOTYzNDcsImV4cCI6MjA4ODc3MjM0N30.vGSYVkIkPrs3IlI4p9SnNZrguStafFLVFLU7qum9a3Y"
 
 export default function Home(){
 
