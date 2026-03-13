47287d9256be23e6fde8f87b9c714d451ce0bc19
Pianta PA <pa4397@gmail.com>
2026-03-13 08:59:26 -0400

    Update App.jsx

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,22 +1,11 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-
 import Home from "./pages/Home"
-import NewsPage from "./pages/NewsPage"
 
-export default function App(){
+function App() {
 
-  return(
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route path="/" element={<Home/>}/>
-        <Route path="/noticia/:slug" element={<NewsPage/>}/>
-
-      </Routes>
-
-    </BrowserRouter>
-
+  return (
+    <Home />
   )
+
 }
+
+export default App
