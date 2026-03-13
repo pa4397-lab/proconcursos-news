7ea4541f2f5fce493df2d4e4d1c1f6b910d1d23c
Pianta PA <pa4397@gmail.com>
2026-03-13 08:48:45 -0400

    Create noticia.js

---
+++ noticia.js
@@ -0,0 +1,45 @@
+const urlParams = new URLSearchParams(window.location.search)
+
+const slug = urlParams.get("slug")
+
+const SUPABASE_URL = "https://svfrmghbnyzkaorpnlqq.supabase.co"
+const SUPABASE_KEY = "SUA_ANON_KEY"
+
+async function loadNews(){
+
+const res = await fetch(
+`${SUPABASE_URL}/rest/v1/news?slug=eq.${slug}&select=*`,
+{
+headers:{
+apikey: SUPABASE_KEY,
+Authorization:`Bearer ${SUPABASE_KEY}`
+}
+})
+
+const data = await res.json()
+
+const news = data[0]
+
+const image = news.image || "https://placehold.co/900x500"
+
+document.getElementById("app").innerHTML = `
+
+<h1 class="text-4xl font-bold mb-6">
+${news.title}
+</h1>
+
+<img src="${image}" class="rounded-lg mb-6"/>
+
+<p class="text-gray-600 mb-6">
+${news.summary}
+</p>
+
+<div class="text-lg leading-relaxed">
+${news.content}
+</div>
+
+`
+
+}
+
+loadNews()
