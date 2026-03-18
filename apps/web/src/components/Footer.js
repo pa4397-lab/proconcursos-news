f8bac6b5228d53af084c9e15bf4977e1c5630afc
Pianta PA <pa4397@gmail.com>
2026-03-18 19:03:25 -0400

    Create Footer.js

---
+++ apps/web/src/components/Footer.js
@@ -0,0 +1,19 @@
+export default function Footer(){
+
+return(
+
+<footer style={{
+marginTop:40,
+background:"#111",
+color:"white",
+textAlign:"center",
+padding:25
+}}>
+
+© {new Date().getFullYear()} ProConcursos News
+
+</footer>
+
+)
+
+}
